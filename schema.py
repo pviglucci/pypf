cb3e059885f4d7861c0d9b36b52d92f448f1d89f
Peter Viglucci <pviglucci@gmail.com>
2017-08-07 18:58:19 -0400

    Marked schema.py as deprecated.

--- schema.py
+++ schema.py
@@ -1,8 +1,12 @@
+"""This is the docstring."""
 import os
 
 
-class TradeMeta:
+class TradeMeta(object):
+    """Deprecated class that will be removed."""
+
     def __init__(self):
+        """Deprecated."""
         self.home_directory = '.'
         self.data_directory = os.path.join(self.home_directory, 'data')
         self.historical_directory = os.path.join(self.home_directory,
